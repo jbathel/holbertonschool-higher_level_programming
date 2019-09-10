c38fec4b115d049068230f418d9722dca3fd74f4
jbathel <jessica.bathel@gmail.com>
2019-09-10 22:54:48 +0000

    Version 4 - removing .format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,13 +4,13 @@ number = random.randint(-10000, 10000)
 
 if number < 0:
     digit = (abs(number) % 10) * -1
-elif number > 0:
+else:
     digit = number % 10
 
 print('Last digit of {} is {} and is '.format(number, digit), end="")
 if digit == 0:
-    print('0'.format(digit))
+    print('0')
 elif digit < 6 and digit != 0:
-    print('less than 6 and not 0'.format(digit))
+    print('less than 6 and not 0')
 elif digit > 5:
-    print('greater than 5'.format(digit))
+    print('greater than 5')
