1d1d8944c3a7f8ae14f0232aa4dfcf247044452e
jbathel <jessica.bathel@gmail.com>
2019-09-11 22:12:58 +0000

    Version 3 - removing extra print statement

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
+        flag = 0
         if ord(c) >= 97 and ord(c) <= 122:
-            c = ord(c) - 32
-            print('{:c}'.format(c), end="")
-        else:
-            print('{:c}'.format(c), end="")
+            flag = 32
+        print('{:c}'.format(ord(c) - flag), end="")
     print()
