4477f22e9621d9f934ac52d211c8f0ec6628d770
jbathel <jessica.bathel@gmail.com>
2019-09-11 22:30:01 +0000

    Version 1

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+def print_last_digit(number):
+    if number < 0:
+        digit = (abs(number) % 10)
+    else:
+        digit = number % 10
+    print('{:d}'.format(digit), end="")
+    return digit
