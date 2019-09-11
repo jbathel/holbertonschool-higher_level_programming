561ed0f10975e81192cd7e47963ee89998b8e265
jbathel <jessica.bathel@gmail.com>
2019-09-11 00:16:56 +0000

    Version 1

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 99):
-    if i < 10:
+    if i < 98:
         print('{0:02d}'.format(int(i)), end=", ")
-    elif i > 10 and i < 98:
-        print('{}'.format(int(i)), end=", ")
     else:
         print('{}'.format(int(i)))
