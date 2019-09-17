5849e954450ec78ed0c2b523f364dce77bc0ccb5
jbathel <jessica.bathel@gmail.com>
2019-09-17 23:27:10 +0000

    Version 3 - adding list check

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in my_list:
-        my_list = my_list[::-1]
-        print('{:d}'.format(my_list[i - 1]))
+    if my_list:
+        for idx in my_list:
+            my_list = my_list[::-1]
+            print('{:d}'.format(my_list[idx - 1]))
