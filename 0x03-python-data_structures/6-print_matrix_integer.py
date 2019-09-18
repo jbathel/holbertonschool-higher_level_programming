d3e546d153087e797c77c7a804faff502642c0c4
jbathel <jessica.bathel@gmail.com>
2019-09-18 02:48:43 +0000

    Version 2 - adding check for matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        for col in range(len(matrix[0])):
-            print('{}'.format(matrix[row][col]), end='')
-            if col + 1 < len(matrix[0]):
-                print(' ', end='')
-        print()
+    if matrix:
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                print('{}'.format(matrix[row][col]), end='')
+                if col + 1 < len(matrix[0]):
+                    print(' ', end='')
+            print()
