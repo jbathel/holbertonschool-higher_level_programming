78eed170940dd4b3aa5d3c148560050891c4ee00
jbathel <jessica.bathel@gmail.com>
2019-09-18 00:44:38 +0000

    Version 1

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list:
+        temp_list = my_list[:]
+        if idx < 0 or idx > len(temp_list) - 1:
+            return temp_list
+        else:
+            temp_list[idx] = element
+            return temp_list
