1aabae2bcaeb81ef8a6b19e0af5b6faf09a082f6
jbathel <jessica.bathel@gmail.com>
2019-09-18 03:00:20 +0000

    Version 4 - adding :d to format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in range(len(matrix)):
             for col in range(len(matrix[0])):
-                print('{}'.format(matrix[row][col]), end='')
+                print('{:d}'.format(matrix[row][col]), end='')
                 if col + 1 < len(matrix[0]):
                     print(' ', end='')
-            print('')
+            print()
