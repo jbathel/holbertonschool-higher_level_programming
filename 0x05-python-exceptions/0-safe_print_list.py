a49415e34e94d3276aa98a5940a30e15de341d60
Jessica Bathel <811@holbertonschool.com>
2019-09-30 22:11:35 +0000

    Version 2 - Fixing pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:       
             print('{}'.format(my_list[i]), end='')
             counter += 1
-            except IndexError:
+        except IndexError:
             pass
     print("")
     return counter
