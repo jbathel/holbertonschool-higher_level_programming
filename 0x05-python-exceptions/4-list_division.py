974cf3026eae09afd6b3a72d3a7ae9afb83ba612
Jessica Bathel <811@holbertonschool.com>
2019-10-01 04:08:41 +0000

    Version 2 - Moved variable inside for loop

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     my_list_3 = []
-    total = 0
     for i in range(list_length):
+        total = 0
         try:
             total = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
