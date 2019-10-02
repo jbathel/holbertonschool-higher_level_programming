6423280af5c1ed2ae0358f1b98b19e1d931183d8
Jessica Bathel <811@holbertonschool.com>
2019-10-02 01:29:44 +0000

    Version 3 - Fixing PEP8

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
     """A simple square class"""
-    def __init__(self,size=0):
+    def __init__(self, size=0):
         """Initialize size attribute"""
         self.__size = size
