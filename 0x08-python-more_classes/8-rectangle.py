973c85620b59c9853b1c4dc0de340a5fed284504
Jessica Bathel <811@holbertonschool.com>
2019-10-07 23:17:00 +0000

    Version 3 - Replacing else with another if statement

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -66,9 +66,7 @@ class Rectangle:
             raise TypeError('rect_1  must be an instance of Rectangle')
         if isinstance(rect_2, Rectangle) is False:
             raise TypeError('rect_2  must be an instance of Rectangle')
-        if rect_1.area() == rect_2.area():
-            return rect_1
         if rect_1.area() >= rect_2.area():
             return rect_1
-        else:
+        if rect_1.area() < rect_2.area():
             return rect_2
