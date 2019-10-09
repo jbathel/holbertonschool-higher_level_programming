1ba8def1eb486413a3e0dde5c76716bb03a7c159
Jessica Bathel <811@holbertonschool.com>
2019-10-09 01:56:15 +0000

    Version 1

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    print('Holberton', sep=',')
+    setattr(magic_string, 'str', getattr(magic_string, 'str', -1) + 1)
+    return 'Holberton' + ', Holberton' * magic_string.str
