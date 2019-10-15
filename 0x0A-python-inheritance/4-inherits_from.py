d8e4626081bbce6150d0c295db8e5a294c4a466b
Jessica Bathel <811@holbertonschool.com>
2019-10-15 21:35:11 +0000

    Version 2 - Correcting syntax error

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
+    """
+    Returns True if object is an instance of a class that
+    inherited (directly or indirectly) from the specified class
+    """
+    return isinstance(obj, a_class) != a_class
