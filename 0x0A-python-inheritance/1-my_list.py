1a490bbb76c5c242f972ff9c93b80333fc04c93a
Jessica Bathel <811@holbertonschool.com>
2019-10-15 18:08:25 +0000

    Version 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class MyList(list):
+    """MyList class that inherits from list"""
+    pass
+
+    def print_sorted(self):
+        """Function that prints a sorted list"""
+        print(sorted(self))
