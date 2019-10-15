d997ccc80bda29ad61e185540916d35670adcc9b
Jessica Bathel <811@holbertonschool.com>
2019-10-15 03:04:38 +0000

    Updated code to handle NaN in matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
     """Function to divide a matrix by a given number"""
+    error = 'matrix must be a matrix (list of lists) of integers/floats'
+
     if not isinstance(matrix, list):
-        raise TypeError(
-                'matrix must be a matrix (list of lists) of integers/floats')
+        raise TypeError(error)
     if not isinstance(div, (int, float)):
         raise TypeError(
                 'div must be a number')
@@ -14,5 +15,11 @@ def matrix_divided(matrix, div):
         if len(row) != len(matrix[0]):
             raise TypeError(
                     'Each row of the matrix must have the same size')
+        if not isinstance(row, list):
+                raise TypeError(error)
+        for i in row:
+            if not isinstance(i, (int, float)):
+                raise TypeError(error)
+
     else:
         return [[round(i / div, 2) for i in row] for row in matrix]
