73a4037621fde1cef55acfc5c5e03ffd89b2c1b7
Jessica Bathel <811@holbertonschool.com>
2019-10-16 01:52:52 +0000

    Version 1

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        """Initialize width and height attributes"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
