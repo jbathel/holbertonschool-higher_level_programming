76677a27c25ab5620cafb669116288294076680b
Jessica Bathel <811@holbertonschool.com>
2019-10-16 03:36:03 +0000

    Version 1

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+class MyInt(int):
+    """MyInt Class"""
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
+
