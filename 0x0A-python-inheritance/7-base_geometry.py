15c8f8e47b7bf31198856ab6d6ef36fed3755605
Jessica Bathel <811@holbertonschool.com>
2019-10-16 01:53:40 +0000

    Version 2 - Changing isinstance to type comparison

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if isinstance(value, int) is False:
+        if type(value) is not int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
