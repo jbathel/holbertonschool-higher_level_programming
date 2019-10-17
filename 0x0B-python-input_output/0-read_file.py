2b04447172e781808efba988cb0905b687ba541f
Jessica Bathel <811@holbertonschool.com>
2019-10-17 17:02:31 +0000

    Replacing filename and gets rid of newline

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """Function that reads a text file (UTF8) and prints it to stdout"""
-    with open("my_file_0.txt", encoding="utf-8") as filename:
-        print(filename.read())
+    with open(filename, encoding="utf-8") as filename:
+        print(filename.read(), end='')
