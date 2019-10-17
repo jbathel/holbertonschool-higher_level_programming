0e2cf2c72be7604d3c273650f465fbfed7e4863c
Jessica Bathel <811@holbertonschool.com>
2019-10-17 18:22:17 +0000

    Version 1

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
 filename = "add_item.json"
 my_list = []
 try:
     my_list = load_from_json_file(filename)
 except:
     my_list = []
-
-for i in range(1, len(sys.agrv)):
-    my_list.append(sys.argv[i])
+for i in range(1, len(argv)):
+    my_list.append(argv[i])
 save_to_json_file(my_list, filename)
