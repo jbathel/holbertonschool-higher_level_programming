462da0a29c8d2761eb27e6ae28c552c478f3b3ee
Jessica Bathel <811@holbertonschool.com>
2019-10-22 21:07:33 +0000

    Updating docstrings

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,7 +6,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-
+    """Square Class"""
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize Attributes"""
         super().__init__(size, size, x, y, id)
