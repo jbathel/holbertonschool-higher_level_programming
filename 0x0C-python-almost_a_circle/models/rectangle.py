938f645a41eaaf8e4516f8f44e67307524636fbe
Jessica Bathel <811@holbertonschool.com>
2019-10-22 21:29:26 +0000

    Updating Errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -51,6 +51,8 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """x attribute"""
+        if not isinstance(value, int):
+            raise TypeError('x must be an integer')
         if value < 0:
             raise ValueError('x must be >= 0')
         self.__x = value
@@ -63,6 +65,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """y attribute"""
+        if not isinstance(value, int):
+            raise TypeError('y must be an integer')
         if value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
