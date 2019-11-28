3644a7863744082176e90ded164fb793795d5489
Jessica Bathel <811@holbertonschool.com>
2019-11-28 05:12:00 +0000

    Version 1

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Script that creates the database hbtn_0d_usa and the table states in the database hbtn_0d_usa on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbthn_0d_usa;
+CREATE table IF NOT EXISTS states(
+    id INT NOT NULL IDENTITY PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
