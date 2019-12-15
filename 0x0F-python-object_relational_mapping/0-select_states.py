5c9b594529d3b81b06a7da92c3873251aa179750
Jessica Bathel <811@holbertonschool.com>
2019-12-15 21:19:54 +0000

    Added 'do not run on import'

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,15 @@
 import MySQLdb
 import sys
 
-
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    password=sys.argv[2],
-    db=sys.argv[3],
-    charset="utf8"
-)
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
 
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
