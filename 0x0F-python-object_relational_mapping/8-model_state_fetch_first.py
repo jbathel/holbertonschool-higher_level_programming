bfd4e1739501d7b60fd7b094e4ec112ebac991d4
Jessica Bathel <811@holbertonschool.com>
2019-12-16 20:56:55 +0000

    Version 1

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+# Script that lists all states from the database hbtn_0e_0_usa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+    state = session.query(State).first()
+    print("{}: {}".format(state.id, state.name))
+    session.close()
