b3b51417b2e9060144621d210c83670b728a701a
Jessica Bathel <811@holbertonschool.com>
2019-12-16 22:35:28 +0000

    Version 1

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Session = sessionmaker(engine)
     session = Session()
     state = session.query(State).filter(State.name.contains(argv[4])).first()
-    if state:
+    if state.name == argv[4]:
         print('{}'.format(state.id))
     else:
         print('Not found')
