d45574c8b91e2dac4da6f19aaf7eefba989c3759
Jessica Bathel <811@holbertonschool.com>
2019-12-16 22:56:19 +0000

    Version 1

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+# Script that lists all states from the database hbtn_0e_0_usa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+    state = State(name='Louisiana')
+    session.add(state)
+    session.commit()
+    print('{}'.format(state.id))
+    session.close()
