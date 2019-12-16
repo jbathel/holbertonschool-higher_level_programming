eb158afc3f155f249d429b0e973c94f7b37a2848
Jessica Bathel <811@holbertonschool.com>
2019-12-16 05:12:50 +0000

    Version 1

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+# Script that lists all states from the database hbtn_0e_0_usa
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class States(Base):
+    """Defines Class States"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
