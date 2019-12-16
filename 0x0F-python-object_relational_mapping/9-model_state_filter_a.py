b926732acc50fceeeabee636c3d60de7b2a73943
Jessica Bathel <811@holbertonschool.com>
2019-12-16 22:00:33 +0000

    Version 1

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+# Script that lists all states from the database hbtn_0e_0_usa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+    results = session.query(State).order_by(State.id).filter(
+        State.name.contains('a')).all()
+    for state in results:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
