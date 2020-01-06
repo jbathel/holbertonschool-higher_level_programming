b971ba867b888b66f82006675bce33891bf1d78c
Jessica Bathel <811@holbertonschool.com>
2020-01-06 20:32:37 +0000

    Added grep and awk

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Script that takes in a URL, sends a request to that URL,
 #and displays the size of the body of the response
-curl -sI http://349bff8a59bb.38.hbtn-cod.io:5000/ | grep -i Content-Length | awk '{ print $2 }'
+curl -sI $1 | grep -i Content-Length | awk '{ print $2 }'
