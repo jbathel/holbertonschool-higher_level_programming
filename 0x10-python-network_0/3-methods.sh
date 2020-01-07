db3dff876a1d252a2173d7d8a836408e98dc418f
Jessica Bathel <811@holbertonschool.com>
2020-01-07 04:16:12 +0000

    Replaced URL with variable

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI 0.0.0.0:5000/route_4 | grep Allow | cut -f2- -d " "
+curl -sI "$1" | grep Allow | cut -f2- -d " "
