adba92df561edf0b667f13fdbfaf85949126aebe
Jessica Bathel <811@holbertonschool.com>
2020-01-13 22:33:41 +0000

    Fixed typo

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
             body = response.read()
         print(body.decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print('Error Code: {}'.format(e.code))
+        print('Error code: {}'.format(e.code))
