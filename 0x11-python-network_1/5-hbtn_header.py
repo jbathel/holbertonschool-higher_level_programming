6c90df7440c210fa85d1ad6b305ffc2be402a4e8
Jessica Bathel <811@holbertonschool.com>
2020-01-13 22:55:32 +0000

    Version 1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+# Python script that fetches https://intranet.hbtn.io/status
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
