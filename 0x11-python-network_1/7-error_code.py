1b595f44ed822ef9b5658dc27a5a3fb56cc562b2
Jessica Bathel <811@holbertonschool.com>
2020-01-13 23:42:51 +0000

    Version 1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+# Python script that fetches https://intranet.hbtn.io/status
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    r = requests.get(sys.argv[1])
+    if r.status_code > 400:
+        print('Error code: {}'.format(r.status_code))
+    else:
+        print(r.text)
