ee585f5434d8729fe7a02a7f0a98ba61eadee949
Jessica Bathel <811@holbertonschool.com>
2020-01-13 23:11:10 +0000

    Version 1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+# Python script that fetches https://intranet.hbtn.io/status
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    r = requests.post(sys.argv[1], data={'email', sys.argv[2]})
+    print(r.text)
