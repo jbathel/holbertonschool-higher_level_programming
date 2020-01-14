3296e568a0bd34df66a68f4d2bd09843ea8a7a16
Jessica Bathel <811@holbertonschool.com>
2020-01-14 05:16:23 +0000

    File setup

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Write a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+    r = requests.get(
+        'https://swapi.co/api/people', params={'search': sys.argv[1]})
+    data = r.json()
+    if data:
+        print("Number of results: {}".format(data['count']))
+        for item in data['results']:
+            print(item['name'])
