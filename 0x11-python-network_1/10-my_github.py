99e4524c3e543524ad866fcb319c0588c6e0edc2
Jessica Bathel <811@holbertonschool.com>
2020-01-14 05:14:14 +0000

    Version 1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id.
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    r = requests.get('https://api.github.com/user',
+                     auth=(sys.argv[1], sys.argv[2]))
+    r_json = r.json()
+    print(r_json.get('id'))
