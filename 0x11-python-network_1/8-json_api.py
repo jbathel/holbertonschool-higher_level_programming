c25564c43080ecdb726210a22be00f7e84ff09a4
Jessica Bathel <811@holbertonschool.com>
2020-01-14 04:12:16 +0000

    Removed if/elif block

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,10 +7,9 @@ if __name__ == '__main__':
     import requests
     import sys
 
-    if len(sys.argv) == 1:
-        data = {'q': ""}
+    data = {'q': ""}
 
-    elif len(sys.argv) > 1:
+    if len(sys.argv) > 1:
         data['q'] = sys.argv[1]
         r = requests.post('http://0.0.0.0:5000/search_user', data)
         try:
