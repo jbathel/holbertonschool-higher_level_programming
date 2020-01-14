fae5680ec3b41615137b083790d45640a3eb608e
Jessica Bathel <811@holbertonschool.com>
2020-01-14 03:51:05 +0000

    Version 1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Write a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    data = {'q': ""}
+
+    if len(sys.argv) > 1:
+        data['q'] = sys.argv[1]
+        r = requests.post('http://0.0.0.0:5000/search_user', data)
+        try:
+            data = r.json()
+            if data:
+                print("[{}] {}".format(data['id'], data['name']))
+            else:
+                print('No result')
+        except Exception:
+            print('Not a valid JSON')
