ce14000d154beea2990df17913ad4a73ecc40afa
Jessica Bathel <811@holbertonschool.com>
2020-02-04 18:07:28 +0000

    Version 1

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 
-if (process.argv[3]) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
