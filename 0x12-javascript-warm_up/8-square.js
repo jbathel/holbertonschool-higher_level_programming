a5fcebe39523066e5ef7a01026fe309c22db894e
Jessica Bathel <811@holbertonschool.com>
2020-02-04 19:55:32 +0000

    Version 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const myVar = 'X';
+const x = process.argv[2];
+
+if (isNaN(x)) {
+  console.log('Missing size');
+}
+for (let i = 0; i < x; i++) {
+  console.log(myVar.repeat(x));
+}
