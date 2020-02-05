b043069571b44e250df131945fb7d46d6d2bf4b1
Jessica Bathel <811@holbertonschool.com>
2020-02-05 03:35:42 +0000

    Version 1

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+let array = [];
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  array = (process.argv.slice(2));
+  const secondLargest = array.sort(function (a, b) { return a - b; })[array.length - 2];
+  console.log(secondLargest);
+}
