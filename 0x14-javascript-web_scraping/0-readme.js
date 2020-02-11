a641a1153a6fa4b8bb02e7c24ccfff3003c60293
Jessica Bathel <811@holbertonschool.com>
2020-02-11 20:27:16 +0000

    Replaced console.error with console.log

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* Script that reads and prints the content of a file.*/
+/* Script that reads and prints the content of a file. */
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
