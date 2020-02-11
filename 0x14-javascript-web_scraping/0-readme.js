ea7a4c75ca168b8ea056738140fe34821dc111d9
Jessica Bathel <811@holbertonschool.com>
2020-02-11 20:01:17 +0000

    Version 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+// path = process.argv[2]
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data.trim());
+});
