94d0b45e0759a1d63afb6561e49b47625e7c7708
Jessica Bathel <811@holbertonschool.com>
2020-02-11 04:42:37 +0000

    Version 1

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+  const revList = [];
+  const length = list.length - 1;
+
+  for (let i = length; i >= 0; i--) {
+    revList.push(list[i]);
+  }
+  return revList;
+};
