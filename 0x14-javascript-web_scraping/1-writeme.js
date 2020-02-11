51cbbf79413e81a041b739242c73e4a7783d59f8
Jessica Bathel <811@holbertonschool.com>
2020-02-11 20:43:46 +0000

    Version 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+// path = process.argv[2]
+const fs = require('fs');
+const data = process.argv[3];
+
+fs.writeFile(process.argv[2], data, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
