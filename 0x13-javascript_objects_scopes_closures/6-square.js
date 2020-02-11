ee98f052d40c86e3f8880fcb2ac7c91c56a57f99
Jessica Bathel <811@holbertonschool.com>
2020-02-11 02:39:14 +0000

    Version 1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+Square.prototype.charPrint = function (c) {
+  if (!c) {
+    c = 'X';
+  }
+  for (let i = 0; i < this.height; i++) {
+    console.log(c.repeat(this.width));
+  }
+};
+
+module.exports = Square;
