53edbf249cbd86d082684b2e60add23b68bad038
Jessica Bathel <811@holbertonschool.com>
2020-02-12 05:46:36 +0000

    Version 1

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], function (error, request, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const tasks = JSON.parse(body);
+    const results = {};
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].completed) {
+        results[tasks[i].userId] += 1;
+      } else {
+        results[tasks[i].userId] = 0;
+      }
+    }
+    console.log(results);
+  }
+});
