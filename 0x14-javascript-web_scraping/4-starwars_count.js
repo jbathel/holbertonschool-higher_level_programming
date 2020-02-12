506e8304dd9f84294faec6962cc41a618d798041
Jessica Bathel <811@holbertonschool.com>
2020-02-12 04:34:40 +0000

    Version 1

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let count = 0;
+    const films = JSON.parse(body).results;
+    for (let i = 0; i < films.length; i++) {
+      for (let j = 0; j < films[i].characters.length; j++) {
+        if (films[i].characters[j].includes('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
