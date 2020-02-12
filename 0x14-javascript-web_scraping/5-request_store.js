39a561c5d376f954d7e9a3010814f0b4c436a5d1
Jessica Bathel <811@holbertonschool.com>
2020-02-12 05:09:45 +0000

    Version 1

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request.get(process.argv[2], function (error, request, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], data, 'utf-8', function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
